222c411ce5e2cff6f5b5fef9b090b0bcaa079253
Lin <2295141009@stu.hqu.edu.cn>
2024-02-19 15:56:26 +0800

    [初始化]

---
+++ packages/client/src/components/Upload-Image.vue
@@ -0,0 +1,20 @@
+<template>
+    <n-upload multiple directory-dnd accept=".jpg .png .jpeg .webp" :max="5">
+        <n-upload-dragger>
+            <div style="margin-bottom: 12px">
+                <n-icon size="48" :depth="3">
+                    <archive-icon />
+                </n-icon>
+            </div>
+            <n-text style="font-size: 16px"> 点击或者拖动文件到该区域来上传 </n-text>
+            <n-p depth="3" style="margin: 8px 0 0 0"> 请不要上传非法图片 o.O </n-p>
+        </n-upload-dragger>
+    </n-upload>
+</template>
+
+<script setup lang="ts">
+import { NUpload, NUploadDragger, NP, NIcon } from 'naive-ui'
+import { ArchiveOutline as ArchiveIcon } from '@vicons/ionicons5'
+</script>
+
+<style scoped></style>
---
+++ packages/client/src/views/Home-View.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="home-container">
+        <n-space vertical>
+            <h3 id="title">柚子茶的图床.</h3>
+            <hr class="hr-twill" />
+            <a id="repo-link" href="https://github.com/Azzellz/my-picture-bed">
+                开源,觉得好的话可以给个star~
+            </a>
+            <upload-image></upload-image>
+        </n-space>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { NSpace } from 'naive-ui'
+import UploadImage from '@/components/Upload-Image.vue'
+</script>
+
+<style scoped>
+#home-container {
+    width: 70%;
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+}
+.hr-twill {
+    border: 0;
+    padding: 3px;
+    background: repeating-linear-gradient(
+        135deg,
+        #a2a9b6 0px,
+        #a2a9b6 1px,
+        transparent 1px,
+        transparent 6px
+    );
+}
+#title {
+    /* font-style: italic; */
+}
+#repo-link {
+    font-size: 10px;
+    color: gray;
+    text-decoration: none;
+}
+</style>
